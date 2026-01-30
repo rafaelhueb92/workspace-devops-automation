3cd235d7d3213b8a60f56435fc6d6d73bcb3ded0
Rafael Hueb <rafaelhueb@192.168.1.6>
2026-01-30 10:34:04 -0300

    include custom data to upgrade version 10 of python

--- infra/vm.tf
+++ infra/vm.tf
@@ -11,6 +11,19 @@ resource "azurerm_linux_virtual_machine" "vm" {
     azurerm_network_interface.nic.id
   ]
 
+  # Config the VM to upgrade the python version
+  custom_data = base64encode(<<-EOF
+    #cloud-config
+    package_update: true
+    packages:
+      - python3.10
+      - python3.10-distutils
+    runcmd:
+      - update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 10
+      - python3 --version
+  EOF
+  )
+
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
