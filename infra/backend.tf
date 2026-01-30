e95d9ff3c213e1602a8d6a195b27dff71542d199
Rafael Hueb <rafaelhueb@192.168.1.6>
2026-01-30 09:22:51 -0300

    correct the storage account name in the backend

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
-    storage_account_name = "tfstatecurso10998"
+    storage_account_name = "tfstatecurso15278" # To get the storage account name run: az storage account list --query "[0].name"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
